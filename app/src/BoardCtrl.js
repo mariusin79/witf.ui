ee071f781ce3b1d67926e5d428ce0636cfffcbb9
Torstein Bjørnstad <tosh@tosh.no>
2014-09-01 23:26:30 +0200

    adding initial styling

--- app/src/BoardCtrl.js
+++ app/src/BoardCtrl.js
@@ -2,9 +2,26 @@ angular.module('pingPongApp')
 	.controller('BoardCtrl', function ($scope) {
 		$scope.addIngredient = function (){
 			if($scope.ingredient.length>0){
-				$scope.ingredients.push($scope.ingredient);
+				$scope.ingredients.push({name: $scope.ingredient});
 				$scope.ingredient = '';
 			}
 		}
-		$scope.ingredients = ["A", "List", "Of", "Ingredients"];
+
+		$scope.ingredients = [
+			{name: "A"},
+			{name: "List"},
+			{name: "Of"},
+			{name: "Ingredients"}
+		];
+
+		$scope.removeIngredient = function(ingredient){
+			var idx = $scope.ingredients.indexOf(ingredient);
+			$scope.ingredients.splice(idx, 1);
+		}
+
+		// hack, move into directive
+		$scope.collapse = function (){
+			var detailDiv = $('.detail');
+			detailDiv.css({'margin-top': 0 - detailDiv.height()});
+		}
 	});
--- app/src/SearchCtrl.js
+++ app/src/SearchCtrl.js
@@ -1,4 +1,9 @@
 angular.module('pingPongApp')
 	.controller('SearchCtrl', function ($scope) {
 		$scope.recipes = ["A", "List", "Of", "Recipes"];
+
+		$scope.expand = function (){
+			var detailDiv = $('.detail');
+			detailDiv.css({'margin-top': 0});
+		}
 	});
